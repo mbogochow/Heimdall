ad6c09e1ee28bcd6c0545e76fe54d2dcb2de927b
Mike Bogochow <mbogochow@hotmail.com>
2021-03-11 00:30:36 -0500

    Fix error for blades that do not have an override_url input

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -164,7 +164,7 @@ $.when( $.ready ).then(function() {
         var apiurl = $('#create input[name=url]').val();
 
         var override_url = $('#create input[name="config[override_url]"]').val();
-        if(override_url.length && override_url != '') {
+        if(override_url && override_url.length && override_url != '') {
             apiurl = override_url;
         }
 
