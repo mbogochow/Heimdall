87e4d129f8bbd77f5533c3ea973135c541869e04
Mike Bogochow <mbogochow@hotmail.com>
2021-03-12 13:24:15 -0500

    Provide override_url input and fix other inputs to be properly sent in POST

--- app/SupportedApps/GitLab/config.blade.php
+++ app/SupportedApps/GitLab/config.blade.php
@@ -1,15 +1,24 @@
 <h2>{{ __('app.apps.config') }} ({{ __('app.optional') }}) @include('items.enable')</h2>
-<h5>{{ __('app.apps.only_admin_account') }}</h5>
 <div class="items">
+    <div class="input">
+        <label>{{ strtoupper(__('app.url')) }}</label>
+        {!! Form::text('config[override_url]', (isset($item) ? $item->getconfig()->override_url : null), array('placeholder' => __('app.apps.override'), 'id' => 'override_url', 'class' => 'form-control')) !!}
+    </div>
     <div class="input">
         <label>Health Token</label>
         <small>Admin Area &raquo; Monitoring &raquo; Health Check</small>
-        {!! Form::text('config[health_apikey]', null, array('placeholder' => 'Health Token', 'id' => 'health_apikey', 'class' => 'form-control config-item')) !!}
+        @php
+            $configKey = 'health_apikey';
+        @endphp
+        {!! Form::text("config[$configKey]", null, array('placeholder' => 'Health Token', 'id' => $configKey, 'class' => 'form-control config-item', 'data-config' => $configKey)) !!}
     </div>
     <div class="input">
         <label>Private API-Read Token</label>
         <small>User Settings &raquo; Access Tokens</small>
-        {!! Form::text('config[private_apikey]', null, array('placeholder' => __('app.apps.apikey'), 'id' => 'private_apikey', 'class' => 'form-control config-item')) !!}
+        @php
+            $configKey = 'private_apikey';
+        @endphp
+        {!! Form::text("config[$configKey]", null, array('placeholder' => __('app.apps.apikey'), 'id' => $configKey, 'class' => 'form-control config-item', 'data-config' => $configKey)) !!}
     </div>
     <div class="input">
         <button style="margin-top: 32px;" class="btn test" id="test_config">Test</button>
